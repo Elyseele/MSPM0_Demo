66d1d158647d8735bedc3ba5d5dff00773477644
auv_mujika <149288217+Elysia1030@users.noreply.github.com>
2025-05-07 18:11:36 +0800

    commit1

---
+++ ADC_Without_FIFO/Debug/ti_msp_dl_config.c
@@ -0,0 +1,317 @@
+/*
+ * Copyright (c) 2023, Texas Instruments Incorporated
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ *  ============ ti_msp_dl_config.c =============
+ *  Configured MSPM0 DriverLib module definitions
+ *
+ *  DO NOT EDIT - This file is generated for the MSPM0G350X
+ *  by the SysConfig tool.
+ */
+
+#include "ti_msp_dl_config.h"
+
+DL_TimerG_backupConfig gWave_TIMBackup;
+
+/*
+ *  ======== SYSCFG_DL_init ========
+ *  Perform any initialization needed before using any board APIs
+ */
+SYSCONFIG_WEAK void SYSCFG_DL_init(void)
+{
+    SYSCFG_DL_initPower();
+    SYSCFG_DL_GPIO_init();
+    /* Module-Specific Initializations*/
+    SYSCFG_DL_SYSCTL_init();
+    SYSCFG_DL_Music_TIM_init();
+    SYSCFG_DL_Wave_TIM_init();
+    SYSCFG_DL_Music_ADC_init();
+    SYSCFG_DL_Wave_ADC_init();
+    SYSCFG_DL_DMA_init();
+    /* Ensure backup structures have no valid state */
+	gWave_TIMBackup.backupRdy 	= false;
+
+}
+/*
+ * User should take care to save and restore register configuration in application.
+ * See Retention Configuration section for more details.
+ */
+SYSCONFIG_WEAK bool SYSCFG_DL_saveConfiguration(void)
+{
+    bool retStatus = true;
+
+	retStatus &= DL_TimerG_saveConfiguration(Wave_TIM_INST, &gWave_TIMBackup);
+
+    return retStatus;
+}
+
+
+SYSCONFIG_WEAK bool SYSCFG_DL_restoreConfiguration(void)
+{
+    bool retStatus = true;
+
+	retStatus &= DL_TimerG_restoreConfiguration(Wave_TIM_INST, &gWave_TIMBackup, false);
+
+    return retStatus;
+}
+
+SYSCONFIG_WEAK void SYSCFG_DL_initPower(void)
+{
+    DL_GPIO_reset(GPIOA);
+    DL_GPIO_reset(GPIOB);
+    DL_TimerG_reset(Music_TIM_INST);
+    DL_TimerG_reset(Wave_TIM_INST);
+    DL_ADC12_reset(Music_ADC_INST);
+    DL_ADC12_reset(Wave_ADC_INST);
+
+
+    DL_GPIO_enablePower(GPIOA);
+    DL_GPIO_enablePower(GPIOB);
+    DL_TimerG_enablePower(Music_TIM_INST);
+    DL_TimerG_enablePower(Wave_TIM_INST);
+    DL_ADC12_enablePower(Music_ADC_INST);
+    DL_ADC12_enablePower(Wave_ADC_INST);
+
+    delay_cycles(POWER_STARTUP_DELAY);
+}
+
+SYSCONFIG_WEAK void SYSCFG_DL_GPIO_init(void)
+{
+
+    DL_GPIO_initPeripheralAnalogFunction(GPIO_HFXIN_IOMUX);
+    DL_GPIO_initPeripheralAnalogFunction(GPIO_HFXOUT_IOMUX);
+
+}
+
+
+static const DL_SYSCTL_SYSPLLConfig gSYSPLLConfig = {
+    .inputFreq              = DL_SYSCTL_SYSPLL_INPUT_FREQ_32_48_MHZ,
+	.rDivClk2x              = 1,
+	.rDivClk1               = 0,
+	.rDivClk0               = 0,
+	.enableCLK2x            = DL_SYSCTL_SYSPLL_CLK2X_DISABLE,
+	.enableCLK1             = DL_SYSCTL_SYSPLL_CLK1_ENABLE,
+	.enableCLK0             = DL_SYSCTL_SYSPLL_CLK0_DISABLE,
+	.sysPLLMCLK             = DL_SYSCTL_SYSPLL_MCLK_CLK0,
+	.sysPLLRef              = DL_SYSCTL_SYSPLL_REF_SYSOSC,
+	.qDiv                   = 1,
+	.pDiv                   = DL_SYSCTL_SYSPLL_PDIV_1
+};
+SYSCONFIG_WEAK void SYSCFG_DL_SYSCTL_init(void)
+{
+
+	//Low Power Mode is configured to be SLEEP0
+    DL_SYSCTL_setBORThreshold(DL_SYSCTL_BOR_THRESHOLD_LEVEL_0);
+    DL_SYSCTL_setFlashWaitState(DL_SYSCTL_FLASH_WAIT_STATE_2);
+
+    
+	DL_SYSCTL_setSYSOSCFreq(DL_SYSCTL_SYSOSC_FREQ_BASE);
+	/* Set default configuration */
+	DL_SYSCTL_disableHFXT();
+	DL_SYSCTL_disableSYSPLL();
+    DL_SYSCTL_setHFCLKSourceHFXTParams(DL_SYSCTL_HFXT_RANGE_32_48_MHZ,10, false);
+    DL_SYSCTL_configSYSPLL((DL_SYSCTL_SYSPLLConfig *) &gSYSPLLConfig);
+    DL_SYSCTL_setULPCLKDivider(DL_SYSCTL_ULPCLK_DIV_1);
+    DL_SYSCTL_setMCLKSource(SYSOSC, HSCLK, DL_SYSCTL_HSCLK_SOURCE_HFCLK);
+
+}
+
+
+
+/*
+ * Timer clock configuration to be sourced by BUSCLK /  (40000000 Hz)
+ * timerClkFreq = (timerClkSrc / (timerClkDivRatio * (timerClkPrescale + 1)))
+ *   40000000 Hz = 40000000 Hz / (1 * (0 + 1))
+ */
+static const DL_TimerG_ClockConfig gMusic_TIMClockConfig = {
+    .clockSel    = DL_TIMER_CLOCK_BUSCLK,
+    .divideRatio = DL_TIMER_CLOCK_DIVIDE_1,
+    .prescale    = 0U,
+};
+
+/*
+ * Timer load value (where the counter starts from) is calculated as (timerPeriod * timerClockFreq) - 1
+ * Music_TIM_INST_LOAD_VALUE = (1ms * 40000000 Hz) - 1
+ */
+static const DL_TimerG_TimerConfig gMusic_TIMTimerConfig = {
+    .period     = Music_TIM_INST_LOAD_VALUE,
+    .timerMode  = DL_TIMER_TIMER_MODE_PERIODIC_UP,
+    .startTimer = DL_TIMER_STOP,
+};
+
+SYSCONFIG_WEAK void SYSCFG_DL_Music_TIM_init(void) {
+
+    DL_TimerG_setClockConfig(Music_TIM_INST,
+        (DL_TimerG_ClockConfig *) &gMusic_TIMClockConfig);
+
+    DL_TimerG_initTimerMode(Music_TIM_INST,
+        (DL_TimerG_TimerConfig *) &gMusic_TIMTimerConfig);
+    DL_TimerG_enableClock(Music_TIM_INST);
+
+
+    DL_TimerG_enableEvent(Music_TIM_INST, DL_TIMERG_EVENT_ROUTE_1, (DL_TIMERG_EVENT_ZERO_EVENT));
+
+    DL_TimerG_setPublisherChanID(Music_TIM_INST, DL_TIMERG_PUBLISHER_INDEX_0, Music_TIM_INST_PUB_0_CH);
+
+
+
+}
+
+/*
+ * Timer clock configuration to be sourced by BUSCLK /  (40000000 Hz)
+ * timerClkFreq = (timerClkSrc / (timerClkDivRatio * (timerClkPrescale + 1)))
+ *   40000000 Hz = 40000000 Hz / (1 * (0 + 1))
+ */
+static const DL_TimerG_ClockConfig gWave_TIMClockConfig = {
+    .clockSel    = DL_TIMER_CLOCK_BUSCLK,
+    .divideRatio = DL_TIMER_CLOCK_DIVIDE_1,
+    .prescale    = 0U,
+};
+
+/*
+ * Timer load value (where the counter starts from) is calculated as (timerPeriod * timerClockFreq) - 1
+ * Wave_TIM_INST_LOAD_VALUE = (0 ms * 40000000 Hz) - 1
+ */
+static const DL_TimerG_TimerConfig gWave_TIMTimerConfig = {
+    .period     = Wave_TIM_INST_LOAD_VALUE,
+    .timerMode  = DL_TIMER_TIMER_MODE_PERIODIC_UP,
+    .startTimer = DL_TIMER_STOP,
+};
+
+SYSCONFIG_WEAK void SYSCFG_DL_Wave_TIM_init(void) {
+
+    DL_TimerG_setClockConfig(Wave_TIM_INST,
+        (DL_TimerG_ClockConfig *) &gWave_TIMClockConfig);
+
+    DL_TimerG_initTimerMode(Wave_TIM_INST,
+        (DL_TimerG_TimerConfig *) &gWave_TIMTimerConfig);
+    DL_TimerG_enableClock(Wave_TIM_INST);
+
+
+    DL_TimerG_enableEvent(Wave_TIM_INST, DL_TIMERG_EVENT_ROUTE_1, (DL_TIMERG_EVENT_ZERO_EVENT));
+
+    DL_TimerG_setPublisherChanID(Wave_TIM_INST, DL_TIMERG_PUBLISHER_INDEX_0, Wave_TIM_INST_PUB_0_CH);
+
+
+
+}
+
+
+/* Music_ADC Initialization */
+static const DL_ADC12_ClockConfig gMusic_ADCClockConfig = {
+    .clockSel       = DL_ADC12_CLOCK_HFCLK,
+    .divideRatio    = DL_ADC12_CLOCK_DIVIDE_1,
+    .freqRange      = DL_ADC12_CLOCK_FREQ_RANGE_32_TO_40,
+};
+SYSCONFIG_WEAK void SYSCFG_DL_Music_ADC_init(void)
+{
+    DL_ADC12_setClockConfig(Music_ADC_INST, (DL_ADC12_ClockConfig *) &gMusic_ADCClockConfig);
+    DL_ADC12_initSingleSample(Music_ADC_INST,
+        DL_ADC12_REPEAT_MODE_ENABLED, DL_ADC12_SAMPLING_SOURCE_AUTO, DL_ADC12_TRIG_SRC_EVENT,
+        DL_ADC12_SAMP_CONV_RES_12_BIT, DL_ADC12_SAMP_CONV_DATA_FORMAT_UNSIGNED);
+    DL_ADC12_configConversionMem(Music_ADC_INST, Music_ADC_ADCMEM_0,
+        DL_ADC12_INPUT_CHAN_0, DL_ADC12_REFERENCE_VOLTAGE_VDDA, DL_ADC12_SAMPLE_TIMER_SOURCE_SCOMP0, DL_ADC12_AVERAGING_MODE_DISABLED,
+        DL_ADC12_BURN_OUT_SOURCE_DISABLED, DL_ADC12_TRIGGER_MODE_TRIGGER_NEXT, DL_ADC12_WINDOWS_COMP_MODE_DISABLED);
+    DL_ADC12_setPowerDownMode(Music_ADC_INST,DL_ADC12_POWER_DOWN_MODE_MANUAL);
+    DL_ADC12_setSampleTime0(Music_ADC_INST,3);
+    DL_ADC12_enableDMA(Music_ADC_INST);
+    DL_ADC12_setDMASamplesCnt(Music_ADC_INST,1);
+    DL_ADC12_enableDMATrigger(Music_ADC_INST,(DL_ADC12_DMA_MEM0_RESULT_LOADED));
+    DL_ADC12_setSubscriberChanID(Music_ADC_INST,Music_ADC_INST_SUB_CH);
+    /* Enable ADC12 interrupt */
+    DL_ADC12_clearInterruptStatus(Music_ADC_INST,(DL_ADC12_INTERRUPT_DMA_DONE));
+    DL_ADC12_enableInterrupt(Music_ADC_INST,(DL_ADC12_INTERRUPT_DMA_DONE));
+    DL_ADC12_enableConversions(Music_ADC_INST);
+}
+/* Wave_ADC Initialization */
+static const DL_ADC12_ClockConfig gWave_ADCClockConfig = {
+    .clockSel       = DL_ADC12_CLOCK_HFCLK,
+    .divideRatio    = DL_ADC12_CLOCK_DIVIDE_1,
+    .freqRange      = DL_ADC12_CLOCK_FREQ_RANGE_32_TO_40,
+};
+SYSCONFIG_WEAK void SYSCFG_DL_Wave_ADC_init(void)
+{
+    DL_ADC12_setClockConfig(Wave_ADC_INST, (DL_ADC12_ClockConfig *) &gWave_ADCClockConfig);
+    DL_ADC12_initSingleSample(Wave_ADC_INST,
+        DL_ADC12_REPEAT_MODE_ENABLED, DL_ADC12_SAMPLING_SOURCE_AUTO, DL_ADC12_TRIG_SRC_EVENT,
+        DL_ADC12_SAMP_CONV_RES_12_BIT, DL_ADC12_SAMP_CONV_DATA_FORMAT_UNSIGNED);
+    DL_ADC12_configConversionMem(Wave_ADC_INST, Wave_ADC_ADCMEM_0,
+        DL_ADC12_INPUT_CHAN_0, DL_ADC12_REFERENCE_VOLTAGE_VDDA, DL_ADC12_SAMPLE_TIMER_SOURCE_SCOMP0, DL_ADC12_AVERAGING_MODE_DISABLED,
+        DL_ADC12_BURN_OUT_SOURCE_DISABLED, DL_ADC12_TRIGGER_MODE_TRIGGER_NEXT, DL_ADC12_WINDOWS_COMP_MODE_DISABLED);
+    DL_ADC12_setPowerDownMode(Wave_ADC_INST,DL_ADC12_POWER_DOWN_MODE_MANUAL);
+    DL_ADC12_enableDMA(Wave_ADC_INST);
+    DL_ADC12_setDMASamplesCnt(Wave_ADC_INST,1);
+    DL_ADC12_enableDMATrigger(Wave_ADC_INST,(DL_ADC12_DMA_MEM0_RESULT_LOADED));
+    DL_ADC12_setSubscriberChanID(Wave_ADC_INST,Wave_ADC_INST_SUB_CH);
+    /* Enable ADC12 interrupt */
+    DL_ADC12_clearInterruptStatus(Wave_ADC_INST,(DL_ADC12_INTERRUPT_DMA_DONE));
+    DL_ADC12_enableInterrupt(Wave_ADC_INST,(DL_ADC12_INTERRUPT_DMA_DONE));
+    DL_ADC12_enableConversions(Wave_ADC_INST);
+}
+
+static const DL_DMA_Config gMusic_DMAConfig = {
+    .transferMode   = DL_DMA_FULL_CH_REPEAT_SINGLE_TRANSFER_MODE,
+    .extendedMode   = DL_DMA_NORMAL_MODE,
+    .destIncrement  = DL_DMA_ADDR_INCREMENT,
+    .srcIncrement   = DL_DMA_ADDR_UNCHANGED,
+    .destWidth      = DL_DMA_WIDTH_HALF_WORD,
+    .srcWidth       = DL_DMA_WIDTH_HALF_WORD,
+    .trigger        = Music_ADC_INST_DMA_TRIGGER,
+    .triggerType    = DL_DMA_TRIGGER_TYPE_EXTERNAL,
+};
+
+SYSCONFIG_WEAK void SYSCFG_DL_Music_DMA_init(void)
+{
+    DL_DMA_setTransferSize(DMA, Music_DMA_CHAN_ID, 1024);
+    DL_DMA_initChannel(DMA, Music_DMA_CHAN_ID , (DL_DMA_Config *) &gMusic_DMAConfig);
+}
+static const DL_DMA_Config gWave_DMAConfig = {
+    .transferMode   = DL_DMA_FULL_CH_REPEAT_SINGLE_TRANSFER_MODE,
+    .extendedMode   = DL_DMA_NORMAL_MODE,
+    .destIncrement  = DL_DMA_ADDR_INCREMENT,
+    .srcIncrement   = DL_DMA_ADDR_UNCHANGED,
+    .destWidth      = DL_DMA_WIDTH_HALF_WORD,
+    .srcWidth       = DL_DMA_WIDTH_HALF_WORD,
+    .trigger        = Wave_ADC_INST_DMA_TRIGGER,
+    .triggerType    = DL_DMA_TRIGGER_TYPE_EXTERNAL,
+};
+
+SYSCONFIG_WEAK void SYSCFG_DL_Wave_DMA_init(void)
+{
+    DL_DMA_initChannel(DMA, Wave_DMA_CHAN_ID , (DL_DMA_Config *) &gWave_DMAConfig);
+}
+SYSCONFIG_WEAK void SYSCFG_DL_DMA_init(void){
+    SYSCFG_DL_Music_DMA_init();
+    SYSCFG_DL_Wave_DMA_init();
+}
+
+
---
+++ ADC_Without_FIFO/adc.h
@@ -0,0 +1,32 @@
+#ifndef ADC_H_
+#define ADC_H_
+
+#include <ti/driverlib/dl_adc12.h>
+
+#define ADC_SIZE         (1024)
+#define TIM_CLK          (40000000)
+
+// 全局缓冲区声明
+extern uint16_t gADCSamples[ADC_SIZE];
+extern uint32_t Sample_Rate;
+extern uint16_t ADC_Sample_size;
+
+// 状态标志（volatile跨文件访问）
+extern volatile uint8_t check_wave_adc;
+extern volatile uint8_t check_music_adc;
+
+// ADC初始化函数
+void ADC_Init(void);
+
+// 定时器频率配置
+void TIM_Set_Fre(GPTIMER_Regs *TIMER, uint32_t Fre);
+
+// ADC采样控制
+void Start_Music_ADC(uint32_t Sample_Rate);
+void Start_Wave_ADC(uint32_t Sample_Rate);
+
+// 中断处理函数声明（需在.c文件中关联）
+void Wave_ADC_INST_IRQHandler(void);
+void Music_ADC_INST_IRQHandler(void);
+
+#endif /* ADC_H_ */
